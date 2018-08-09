3dffc7ea8dee1d7081fd448461f9940782335f87
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-08-10 09:48:24 +1200

    Remove reference to SiteTree in tests

--- tests/DataObjects/DogPage.php
+++ tests/DataObjects/DogPage.php
@@ -6,12 +6,17 @@ namespace BatchWrite\Tests;
  * Class DogPage
  * @package BatchWrite\Tests
  */
-class DogPage extends \SiteTree implements \TestOnly
+class DogPage extends \DataObject implements \TestOnly
 {
     /**
      * @var array
      */
     public static $db = array(
+        'Title' => 'Varchar',
         'Author' => 'Varchar',
+    );
+
+    private static $extensions = array(
+        "Versioned('Stage', 'Live')"
     );
 }
