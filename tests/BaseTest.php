8c176c3b234fd26ab3f2021f7320948a4bdc39b9
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-08-13 15:24:20 +1200

    Add Faker as dev dependency for test data

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,6 +2,8 @@
 
 namespace LittleGiant\BatchWrite\Tests;
 
+use Faker\Factory;
+use Faker\Generator;
 use LittleGiant\BatchWrite\Tests\DataObjects\Animal;
 use LittleGiant\BatchWrite\Tests\DataObjects\Batman;
 use LittleGiant\BatchWrite\Tests\DataObjects\Cat;
@@ -36,6 +38,24 @@ abstract class BaseTest extends SapphireTest
      * @var bool
      */
     protected $usesDatabase = true;
+
+    /**
+     * @var Generator
+     */
+    protected $faker;
+
+    /**
+     * BaseTest constructor.
+     * @param null $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->faker = Factory::create();
+    }
 
     /**
      * @inheritdoc
